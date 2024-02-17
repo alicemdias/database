d72f0a83ef1d1b48a9f107f64604ce0e32e3f6be
alicemdias <alicemdias@poli.ufrj.br>
2024-02-17 19:26:00 +0000

    adding rows to little table

--- little_lemon1.sql
+++ little_lemon1.sql
@@ -1,2 +1,21 @@
 CREATE TABLE Starters(StarterName VARCHAR(100) NOT NULL PRIMARY KEY, Cost Decimal(3,2), StarterType VARCHAR(100) DEFAULT 'Mediterranean');
 
+/*
+
+Use the REPLACE statement to insert a new data record with the following details. 
+
+The ‘StarterName’ = Cheese bread
+
+The ‘Cost’ = 9.50 and 
+
+The ‘StarterType’ = Indian 
+
+REPLACE INTO Starters (StarterName, Cost, StarterType) 
+VALUES ("Cheese Bread", 9.50, "Indian");
+
+Use the REPLACE statement to change the cost of the Cheese bread from $9.50 to $9.75.
+
+REPLACE INTO Starters (StarterName, Cost, StarterType) 
+VALUES ("Cheese Bread", 9.75, "Indian");
+
+*/
