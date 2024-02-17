193fffc42c4049c248c9206179482fd7a81dc6a1
alicemdias <alicemdias@poli.ufrj.br>
2024-02-17 19:02:43 +0000

    filtering the table

--- lucky_shrub_db.sql
+++ lucky_shrub_db.sql
@@ -83,3 +83,30 @@ VALUES(1,3,"In Progress",200),
 (14,5,"Not Recieved",300), 
 (14,4,"Not Recieved",250); 
 
+/*
+Use the ANY operator to identify all employees with the Order Status status 'Completed'. 
+
+SELECT EmployeeId, EmployeeName  
+FROM employees  
+WHERE EmployeeID = ANY (SELECT EmployeeID FROM employee_orders WHERE Status='Completed'); 
+
+Use the ALL operator to identify the IDs of employees who earned a handling cost of "more than 20% of the order value" from all orders they have handled.
+
+SELECT EmployeeID,HandlingCost 
+FROM employee_orders  
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal/100 * 20) FROM orders); 
+
+Use the GROUP BY clause to summarize the duplicate records with the same column values into a single record by grouping them based on those columns.
+
+SELECT EmployeeID,HandlingCost   
+FROM employee_orders   
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal/100 * 20) FROM orders) GROUP BY EmployeeID,HandlingCost;
+
+Use the HAVING clause to filter the grouped data in the subquery that you wrote in task 3 to filter the 20% OrderTotal values to only retrieve values that are more than $100.
+
+SELECT EmployeeID,HandlingCost 
+FROM employee_orders  
+WHERE HandlingCost > ALL(SELECT ROUND(OrderTotal/100 * 20) AS twentyPercent FROM orders  GROUP BY OrderTotal  
+HAVING twentyPercent > 100)  GROUP BY EmployeeID,HandlingCost;   
+
+*/
